0d0b5a2fc37d2b19e5cac1a97286d0cf4aeb54fa
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-08-09 13:13:45 +0300

    Array

--- Chapter_1/Celsius/Tasks_1.2/1_15_Celsius_modified.c
+++ Chapter_1/Celsius/Tasks_1.2/1_15_Celsius_modified.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
-void fahr_to_celsium();
+float fahr_to_celsium (float fahr);
+void print_fahr_to_celsium_table ();
 
-void fahr_to_celsium (){
+int main () {
+	print_fahr_to_celsium_table();
+	return 0;
+}
+
+void print_fahr_to_celsium_table (){
 	float fahr, celsius;
 	
 	printf("Fahrengeit into Celsius table\n");
-	for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
-	
-		celsius = (5.0/9.0) * (fahr - 32.0);
+	for (fahr = 0; fahr <= 300; fahr = fahr + 20) {	
+		celsius = fahr_to_celsium(fahr);
 		printf("%4.0F%6.1F\n", fahr, celsius);
 	}
 }
 
-int main () {
-	fahr_to_celsium();
-	return 0;
+float fahr_to_celsium (float fahr) {
+	float celsius;
+	celsius = (5.0/9.0) * (fahr - 32.0);
+	return celsius;
 }
---
+++ Chapter_1/Celsius/Tasks_1.2/Array.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#define N 5
+
+int min(int array[]) {
+	int min, i;
+	min = array[0];
+	for (i = 0; i < N; i++)
+		if (min > array[i])
+			min = array[i];	
+	return min;
+}
+
+int max(int array[]) {
+	int max, i;
+	max = array[0];
+	for (i = 0; i < N; i++)
+		if (max < array[i])
+			max = array[i];
+	return max;
+}
+
+int sum(int array[]) {
+	int sum, i;
+	sum = 0;
+	for (i = 0; i < N; i++)
+		sum = sum + array[i];
+	return sum;
+}
+
+void insert_array(int array[]) {
+	int i;
+	for (i = 1; i < N; i++) {
+		int k = i;
+		while (k > 0 && array[k - 1] > array[k]) {
+			int tmp = array[k - 1];
+			array[k - 1] = array[k];
+			array[k] = tmp;
+			k = k - 1;
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	int i;
+	int array[N];
+	printf ("Enter any values:\n");
+	for (i = 0; i < N; i++)
+		scanf ("%d", &array[i]);
+	printf("Your array is:");
+	for (i = 0; i < N; i++)
+		printf(" %d", array[i]);
+	printf("\n");
+	printf("Your sorted array is:");
+	insert_array(array);
+	for (i = 0; i < N; i++)
+		printf(" %d", array[i]);
+	printf("\n");
+	printf("Min element of array: %d\n", min(array));
+	printf("Max element of array: %d\n", max(array));
+	printf("Sum of array's elements: %d\n", sum(array));
+	return 0;
+}
