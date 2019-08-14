2839415f5cd265bbe081480e0f59eaf31dfa257f
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-08-14 16:23:47 +0300

    Histogramm_v_1.1

--- Chapter_1/Celsius/Tasks_1.2/1_13_Histogram_of_length_of_words.c
+++ Chapter_1/Celsius/Tasks_1.2/1_13_Histogram_of_length_of_words.c
@@ -9,22 +9,33 @@ int main() {
 	while ((c = getchar()) != EOF){
 		nc++;
 		if (c == ' ' || c == '\n' || c == '\t'){
+			if (nc > 15) {
+				printf("You entered too long word. Please try again\n");
+				nc = 0;
+				continue;
+			}	
 			array[nc - 1]++;
-			nc = 0;
+			nc = 0;		
 		}
 	}
-
 	for (j = 0; j <= 15; j++) {
 		if (array[j] == 0)
 			continue;
 		if (j < 10)
-			printf(" %d: ", j);		
+			printf(" %d| ", j);		
 		else
-			printf("%d: ", j);			
+			printf("%d| ", j);			
 		for (int k = 0; k < array[j]; k++)
-			printf("%c", '*');
-		printf("\n");
+			printf("%c ", '*');
+		printf("\n");		
 	}
+	
+	printf("%s", "---------------------------------------");
+	printf("%c", '>');
+	printf("\n");
+	printf("%s", "   ");
+	for (j = 1; j <= 15; j++)
+		printf(" %d", j);
 
 	return 0;
 }
