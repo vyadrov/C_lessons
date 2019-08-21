e6b7241edcfdff82f79a04358e699f4966dcd0dd
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-08-21 16:48:16 +0300

    2_2_Getline_loop_v1.1

--- Chapter_2/2_2_Getline_loop.c
+++ Chapter_2/2_2_Getline_loop.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #define N 20
 
-void wordscan(int array[N]) {
+void mygetline(int array[N]) {
 	int c, i;
 	for (i = 0; i < N; i++)
-		//array[i] = 0;
-	while ((c = getchar()) != '\n') {
-		if (c != EOF)
-			putchar(c);
+		array[i] = 0;
+	i = 0;
+	while ((c = getchar()) != EOF) {
+		if (i >= N - 1) {
+			printf("You entered too long word.\n");
+			break;
+		}
+		if (c != '\n') {
+			array[i] = c;
+			i++;
+		}
+		else
+			break;
 	}
 }
 
 int main () {
 	int array[N];
-	wordscan(array);
-
+	mygetline(array);
+	for (int i = 0; array[i] != 0; i++)
+		putchar(array[i]);
+	putchar('\n');
 	return 0;
 }
