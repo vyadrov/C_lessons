472cafdc1542da817b202d9a0d850082b55dade8
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-08-22 15:40:08 +0300

    Determine Rangrs v1.2

--- Chapter_2/2_1_Determine_Ranges.c
+++ Chapter_2/2_1_Determine_Ranges.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 #include <math.h>
 
+#define N 8
+
 void determineRanges(int x) {
-	char  a, b;
-	short c, d;
-	int e, f;
-	long g, h;
-	unsigned char m;
-	unsigned short n;
-	long int k, l;
-	unsigned int o;
-	unsigned long p;
+	char  min_signed_char, max_signed_char;
+	short min_signed_short, max_signed_short;
+	int min_signed_int, max_signed_int;
+	long min_signed_long, max_signed_long;
+	unsigned char max_unsigned_char;
+	unsigned short max_unsigned_short;
+	long int min_signed_long_int, max_signed_long_int;
+	unsigned int max_unsigned_int;
+	unsigned long max_unsigned_long;
 	
-	a = -(pow(2, (sizeof(char)*8 - 1)));
-	b = pow(2, (sizeof(char)*8 - 1)) - 1;
-	c = -(pow(2, (sizeof(short)*8 - 1)));
-	d = pow(2, (sizeof(short)*8 - 1)) - 1;
-	e = -(pow(2, (sizeof(int)*8 - 1)));
-	f = pow(2, (sizeof(int)*8 - 1)) - 1;
-	g = -(pow(2, (sizeof(long)*8 - 1)));
-	h = pow(2, (sizeof(long)*8 - 1)) - 1;
-	k = -(pow(2, (sizeof(long int)*8 - 1)));
-	l = pow(2, (sizeof(long int)*8 - 1)) - 1;
-	m = pow(2, sizeof(unsigned char)*8) - 1;
-	n = pow(2, sizeof(unsigned short)*8) - 1;
-	o = pow(2, sizeof(unsigned int)*8) - 1;
-	p = pow(2, sizeof(unsigned long)*8) - 1;
-	printf("Minimum Signed Char %hhd\n", a);
-	printf("Maximum Signed Char % hhd\n", b);
-	printf("Minimum Signed Short %hd\n", c);
-	printf("Maximum Signed Short % hd\n", d);
-	printf("Minimum Signed Int %d\n", e);
-	printf("Maximum Signed Int % d\n", f);
-	printf("Minimum Signed Long %ld\n", g);
-	printf("Maximum Signed Long % ld\n", h);
-	printf("Minimum Signed Long Int %ld\n", k);
-	printf("Maximum Signed Long Int % ld\n\n", l);
-	printf("Maximum Unsigned Char %hhu\n", m);
-	printf("Maximum Unsigned Short %hu\n", n);
-	printf("Maximum Unsigned Int %u\n", o);
-	printf("Maximum Unsigned Long %lu\n", p);
+	min_signed_char = -(pow(2, (sizeof(char)*N - 1)));
+	max_signed_char = pow(2, (sizeof(char)*N - 1)) - 1;
+	min_signed_short = -(pow(2, (sizeof(short)*N - 1)));
+	max_signed_short = pow(2, (sizeof(short)*N - 1)) - 1;
+	min_signed_int = -(pow(2, (sizeof(int)*N - 1)));
+	max_signed_int = pow(2, (sizeof(int)*N - 1)) - 1;
+	min_signed_long = -(pow(2, (sizeof(long)*N - 1)));
+	max_signed_long = pow(2, (sizeof(long)*N - 1)) - 1;
+	min_signed_long_int = -(pow(2, (sizeof(long int)*N - 1)));
+	max_signed_long_int = pow(2, (sizeof(long int)*N - 1)) - 1;
+	max_unsigned_char = pow(2, sizeof(unsigned char)*N) - 1;
+	max_unsigned_short = pow(2, sizeof(unsigned short)*N) - 1;
+	max_unsigned_int = pow(2, sizeof(unsigned int)*N) - 1;
+	max_unsigned_long = pow(2, sizeof(unsigned long)*N) - 1;
+	printf("Minimum Signed Char %hhd\n", min_signed_char);
+	printf("Maximum Signed Char % hhd\n", max_signed_char);
+	printf("Minimum Signed Short %hd\n", min_signed_short);
+	printf("Maximum Signed Short % hd\n", max_signed_short);
+	printf("Minimum Signed Int %d\n", min_signed_int);
+	printf("Maximum Signed Int % d\n", max_signed_int);
+	printf("Minimum Signed Long %ld\n", min_signed_long);
+	printf("Maximum Signed Long % ld\n", max_signed_long);
+	printf("Minimum Signed Long Int %ld\n", min_signed_long_int);
+	printf("Maximum Signed Long Int % ld\n\n", max_signed_long_int);
+	printf("Maximum Unsigned Char %hhu\n", max_unsigned_char);
+	printf("Maximum Unsigned Short %hu\n", max_unsigned_short);
+	printf("Maximum Unsigned Int %u\n", max_unsigned_int);
+	printf("Maximum Unsigned Long %lu\n", max_unsigned_long);
 }
 
 
