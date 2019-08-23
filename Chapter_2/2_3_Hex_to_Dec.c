ab2f5a4e4f9163155eb122e502fe471095c16828
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-08-23 15:00:57 +0300

    2_3_Hex_to_Dec_v1.0

---
+++ Chapter_2/2_3_Hex_to_Dec.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define ARRAY_SIZE 10
+
+
+
+int htoi(char s[ARRAY_SIZE]) {
+	int input_digit, array_element_index, coefficient;
+	coefficient = 0;
+	for (array_element_index = 0; (input_digit = s[array_element_index]) != '\0'; ++array_element_index) {
+        coefficient = coefficient * 16;
+        if (input_digit == '0') {
+            input_digit = s[++array_element_index];
+            if (input_digit != 'x' && input_digit != 'X')
+                --array_element_index;
+            else if (s[array_element_index] == 'x' && s[array_element_index] == 'X')
+                ++array_element_index;                 
+        }
+        else if (input_digit >= '0' && input_digit <= '9')
+            input_digit = input_digit + input_digit - '0';
+        else if (input_digit >= 'a' && input_digit <= 'f')
+            coefficient = coefficient + 10 + (input_digit - 'a');
+        else if (input_digit >= 'A' && input_digit <= 'F')
+            coefficient = coefficient + 10 + (input_digit - 'A');
+        else
+            return coefficient;
+    }
+    return coefficient;
+}
+
+void scan_hex_number(char s[ARRAY_SIZE]) {
+	int array_element_index, input_digit;
+	printf("Please enter any Hex number:\n");
+	for (array_element_index = 0; (input_digit = getchar()) != '\n'; array_element_index++)
+		s[array_element_index] = input_digit;
+	s[array_element_index] = '\0';
+	printf("%s in Dec system is: %d\n", s, htoi(s));
+}
+
+int main (void) {
+	char s[ARRAY_SIZE];
+	scan_hex_number(s);
+	htoi(s);
+}
