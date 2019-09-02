6a342fbcfd55cf97100cd276e14c581d783f0342
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-09-02 12:27:26 +0300

    2_5_First_location_in_string_of_consist_char_v1.9

--- Chapter_2/2_5_First_location_in_string_of_consist_char.c
+++ Chapter_2/2_5_First_location_in_string_of_consist_char.c
@@ -3,37 +3,34 @@
 #include <string.h>
 
 #define ARRAY_SIZE 30
-#define STRINGIFY(x)
 
 void scan(char string1[ARRAY_SIZE], char string2[ARRAY_SIZE]) {
     printf("Please enter any first word:\n");
-    scanf("%[^\n]" STRINGIFY(ARRAY_SIZE) "s", string1);
-    //fgets(string1, ARRAY_SIZE, stdin);
+    fgets(string1, ARRAY_SIZE, stdin);
 
-    if (strlen(string1) >= ARRAY_SIZE) {
+    if (strlen(string1) >= ARRAY_SIZE - 1) {
         printf("You entered too long number. Please try again\n");
         exit(0);
     }
 
     printf("Please enter any second word:\n");
-        scanf("%" STRINGIFY(ARRAY_SIZE) "s", string2);
-    //fgets(string2, ARRAY_SIZE, stdin);
+    fgets(string2, ARRAY_SIZE, stdin);
 
-    if (strlen(string2) >= ARRAY_SIZE) {
+    if (strlen(string2) >= ARRAY_SIZE - 1) {
         printf("You entered too long number. Please try again\n");
         exit(0);
     }
 }
 
 int comparison_of_characters(char string1[], char string2[]) {
-    int i, j;    
+    int i, j;
 
     for (i = 0; string1[i] != '\0'; i++) {
         for (j = 0; string2[j] != '\0'; j++) {
             if (string1[i] == string2[j] && string1[i] !='\n') {
                 return i;
-            }                      
-        }        
+            }
+        }
     }
     return -1;
 }
@@ -45,10 +42,10 @@ int main(void) {
     scan(string1, string2);
 
     int result = comparison_of_characters(string1, string2);
-    if(result == -1) 
+    if(result == -1)
         printf("No matches\n");
     else
         printf("Location of first consist symbol between string1 and string2 is: %d, symbol - '%c', ASCII code = %d\n", result + 1, string1[result], string1[result]);
-    
+
     return 0;
 }
