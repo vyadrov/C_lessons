6b6d635074c5315eeca99aa42472a9686d4d9ceb
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-09-05 15:21:39 +0300

    2_6_setbits_v1.1

--- Chapter_2/2_6_setbits.c
+++ Chapter_2/2_6_setbits.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
 unsigned setbits(unsigned x, unsigned p, unsigned n, unsigned y) {
-  int k, z;
-  z = ~(~0 << n); //set "1" to first n bits
-  y = y & z; // set "0" to higher bits
-  k = ~(z << (p - n + 1)); // set n bits to "0" at position p
-  x = x & k; // set n bits to "0" at position p
-  y = y << (p - n + 1); // shift left to p index
-  x = x | y; // final result
-  return x;
+    int k, z;
+
+    z = ~(~0 << n); //set "1" to first n bits
+    y = y & z; // set "0" to higher bits
+    k = ~(z << (p - n + 1)); // set n bits to "0" at position p
+    x = x & k; // set n bits to "0" at position p
+    y = y << (p - n + 1); // shift left to p index
+    x = x | y; // final result
+    
+    return x;
 }
 
 int main() {
-  unsigned x, p, n, y;
-  printf("Please enter any number 'x':\n");
-  scanf("%u", &x);
-  printf("Please enter start position 'p' in number 'x':\n");
-  scanf("%u", &p);
-  printf("Please enter 'n' bits:\n");
-  scanf("%u", &n);
-  printf("Please enter any number 'y':\n");
-  scanf("%u", &y);
-  x = setbits(x, p, n, y);
-  printf("Final number is: %u\n", x);
-  return 0;
+    unsigned x, p, n, y;
+
+    printf("Please enter any number 'x':\n");
+    scanf("%u", &x);
+    printf("Please enter start position 'p' in number 'x':\n");
+    scanf("%u", &p);
+    printf("Please enter 'n' bits:\n");
+    scanf("%u", &n);
+    printf("Please enter any number 'y':\n");
+    scanf("%u", &y);
+    x = setbits(x, p, n, y);
+    printf("Final number is: %u\n", x);
+
+    return 0;
 }
