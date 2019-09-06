d31cb1bd1cff20e8a3ae4a6c9b235e315522216d
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-09-06 15:16:16 +0300

    2_6_setbits_v1.2

--- Chapter_2/2_6_setbits.c
+++ Chapter_2/2_6_setbits.c
@@ -9,15 +9,28 @@ unsigned setbits(unsigned x, unsigned p, unsigned n, unsigned y) {
     x = x & k; // set n bits to "0" at position p
     y = y << (p - n + 1); // shift left to p index
     x = x | y; // final result
-    
+
     return x;
+}
+
+void print_binary(unsigned x) {    
+    
+    for (int i = sizeof(x) * 8 - 1; i >= 0; i--) {
+        printf("%d", (x & (1 << i)) >> i);
+    }
+
+    printf("\n");
+
+    return;
 }
 
 int main() {
     unsigned x, p, n, y;
 
-    printf("Please enter any number 'x':\n");
+    printf("Please enter any number 'x' in Dec system:\n");
     scanf("%u", &x);
+    printf("'x' in Binary system:\n");
+    print_binary(x);
     printf("Please enter start position 'p' in number 'x':\n");
     scanf("%u", &p);
     printf("Please enter 'n' bits:\n");
@@ -25,7 +38,9 @@ int main() {
     printf("Please enter any number 'y':\n");
     scanf("%u", &y);
     x = setbits(x, p, n, y);
-    printf("Final number is: %u\n", x);
+    printf("Final number 'x' in Dec system is: %u\n", x);
+    printf("Final number 'x' in Binary system:\n");
+    print_binary(x);
 
     return 0;
 }
