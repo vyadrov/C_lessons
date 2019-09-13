6142594a921d29decf9bd28577c6867c38f3e784
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-09-13 14:22:32 +0300

    Add time compare for classic and binsearch methods

--- Chapter_3/3_1_binsearch.c
+++ Chapter_3/3_1_binsearch.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <time.h>
 
-#define ARRAY_SIZE 12
+#define ARRAY_SIZE 1000000
 
 
 int binsearch(int x, int v[], int n) {
@@ -16,22 +17,42 @@ int binsearch(int x, int v[], int n) {
         else
             low = mid + 1;   
     }
-    if (x == v[low])
-        return low;
+    if (x == v[high])
+        return high;
     else
         return - 1;   
 }
 
+int classic_search_method(int x, int v[], int n) {
+    int i;
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        if (x == v[i])
+            return i;
+    }
+        return -1;
+
+}
+
 int main(void) {
     int a;
-    int array[ARRAY_SIZE] = {2, 3, 4, 5, 9, 13, 16, 19, 20, 25, 30, 45};
+    int array[ARRAY_SIZE];
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%d ", array[i]);
+        array[i] = i * 2;
+        //printf("%d ", array[i]);
     }
     printf("\n");
     printf("Please enter number 'x' to find in array:\n");
     scanf("%d", &a);
-    printf("%d",binsearch(a,array,ARRAY_SIZE));
+    clock_t tic = clock();
+    printf("Index of 'x' in array is: %d\n",binsearch(a,array,ARRAY_SIZE));
+    clock_t toc = clock();
+    printf("Elapsed: %f seconds for binsearch method\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+    printf("Please enter number 'x' to find in array:\n");
+    scanf("%d", &a);
+    clock_t pic = clock();
+    printf("Index of 'x' in array is: %d\n", classic_search_method(a, array, ARRAY_SIZE));
+    clock_t poc = clock();
+    printf("Elapsed: %f seconds for classic search method\n", (double)(poc - pic) / CLOCKS_PER_SEC);
 
     return 0;
 }
