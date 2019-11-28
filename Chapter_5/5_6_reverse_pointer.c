ec4e78efe8a2cede25410e4b186474fdd66f0a0a
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-11-28 12:27:01 +0200

    Correct name of i variable, change it to type char and add print before reverse

--- Chapter_5/5_6_reverse_pointer.c
+++ Chapter_5/5_6_reverse_pointer.c
@@ -7,19 +7,21 @@
 
 void reverse_pointer(char *string) {
 
-    char *temp;
-    int i;
+    char *temp, temp_symbol;
+    
 
     for (temp = string + strlen(string) - 1; string < temp; string++, temp--) {
-        i = *string;
+        temp_symbol = *string;
         *string = *temp;
-        *temp = i;
+        *temp = temp_symbol;
     }
 }
 
 int main(void) {
     
     char string[ARRAY_SIZE] = "abcde";
+
+    printf("String before reverse: %s\n", string);
     reverse_pointer(string);
-    printf("%s\n", string);
+    printf("String after reverse: %s\n", string);
 }
