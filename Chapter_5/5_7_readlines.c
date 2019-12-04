be14b0a0b63f228db1d091a7df84a43e63cfa871
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2019-12-04 12:31:51 +0200

    5_7_readlines_add space

--- Chapter_5/5_7_readlines.c
+++ Chapter_5/5_7_readlines.c
@@ -57,7 +57,7 @@ int readlines(char *lineptr[], char lines[], int maxlines) {
 /* writelines: write output lines */
 void writelines(char *lineptr[], int nlines) {
     int i;
-    for(i = 0; i < nlines; i++)
+    for (i = 0; i < nlines; i++)
         printf("%s\n", lineptr[i]);
 }
 
