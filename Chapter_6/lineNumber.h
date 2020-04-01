0d93b8d310e0146f135991fd26aa1169f70c27f0
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2020-04-01 17:03:07 +0300

    remove old files

--- Chapter_6/lineNumber.h
+++
@@ -1,8 +0,0 @@
-typedef struct list {
-    int value;
-    struct list *next;
-} lineNumber;
-
-lineNumber *getLast(lineNumber *head);
-void pushBack(lineNumber **head, int);
-void printLinkedList(const lineNumber *head);
