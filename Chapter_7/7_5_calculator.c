39011244f0d7eed31013187366957b5a24a55fe9
Vitalii Iadrov <vitalii.iadrov@globallogic.com>
2020-04-24 16:30:05 +0300

    Add 7_5_calculator.c

--- Chapter_7/7_5_calculator.c
+++ Chapter_7/7_5_calculator.c
@@ -10,14 +10,21 @@
 #define MAXVAL 100
 #define BUFSIZE 100
 
-#define POWER 1
-#define SIN 2
-#define COS 3
-#define EXP 4
-#define CLEAN 5
-#define DOUBLE 6
-#define SWAP 7
-#define TOP 8
+enum operations {
+    POWER = 1,
+    SIN,
+    COS,
+    EXP,
+    CLEAN,
+    DOUBLE,
+    SWAP,
+    TOP,
+    ADD,
+    DIFF,
+    MOD,
+    MULTIPLE,
+    DIVIDE
+};
 
 #define POWER_STRING "pow"
 #define SIN_STRING "sin"
@@ -27,6 +34,11 @@
 #define DOUBLE_STRING "d"
 #define SWAP_STRING "s"
 #define TOP_STRING "t"
+#define ADD_STRING "+"
+#define DIFF_STRING "-"
+#define MOD_STRING "\%"
+#define MULTIPLE_STRING "*"
+#define DIVIDE_STRING "/"
 
 int stackPosition = 0;
 double valueStack[MAXVAL];
@@ -34,24 +46,22 @@ char bufForUngetch[BUFSIZE];
 int freePositionInBuf = 0;
 
 void push(double f) {
-    if(stackPosition < MAXVAL)
+    if (stackPosition < MAXVAL)
         valueStack[stackPosition++] = f;
     else
-        printf("error:stack full, cant push %g\n",f);
+        printf("error:stack full, cant push %g\n", f);
 }
 
 double pop(void) {
     if (stackPosition > 0)
         return valueStack[--stackPosition];
-    else
-    {
-        printf("error: stack empty\n");
-        return 0.0;
-    }
+
+    printf("error: stack empty\n");
+    return 0.0;
 }
 
-int getOperationCode(char* string) {
-    double operator_1, operator_2;
+enum operations getOperationCode(char* string) {
+
     if (strcmp(string, POWER_STRING) == 0) {
         return POWER;
     }
@@ -76,6 +86,22 @@ int getOperationCode(char* string) {
     else if (strcmp(string, TOP_STRING) == 0) {
         return TOP;
     }
+    else if (strcmp(string, ADD_STRING) == 0) {
+        return ADD;
+    }
+    else if (strcmp(string, DIFF_STRING) == 0) {
+        return DIFF;
+    }
+    else if (strcmp(string, MOD_STRING) == 0) {
+        return MOD;
+    }
+    else if (strcmp(string, MULTIPLE_STRING) == 0) {
+        return MULTIPLE;
+    }
+    else if (strcmp(string, DIVIDE_STRING) == 0) {
+        return DIVIDE;
+    }
+    
     return -1;
 }
 
@@ -90,77 +116,73 @@ int main(void) {
         if (sscanf(string, " %lf", &value) == 1)
             push(value);
         else if (sscanf(string, "%s", buf)) {
-            for (type = buf ; *type; type++) {
-                switch (*type) {
-                    case NUMBER:
-                        push(atof(string));
-                        break;
-                    case NAME:
-                        opcode = getOperationCode(string);
-                        switch (opcode){
-                            case POWER:
-                                    operator_2 = pop();
-                                    push(pow(pop(), operator_2));
-                                    break;
-                            case SIN:
-                                    push(sin(pop()));
-                                    break;
-                            case COS:
-                                    push(cos(pop()));
-                                    break;
-                            case EXP:
-                                    operator_2 = pop();
-                                    push(exp(pop()));
-                                    break;
-                            case CLEAN:
-                                    stackPosition = 0;
-                                    break;
-                            case DOUBLE:
-                                    operator_2 = pop();
-                                    push(operator_2);
-                                    push(operator_2);
-                                    break;
-                            case SWAP:
-                                    operator_1 = pop();
-                                    operator_2 = pop();
-                                    push(operator_1);
-                                    push(operator_2);
-                                    break;
-                            case TOP:
-                                    operator_2 = pop();
-                                    printf("\t%-.8g\n", operator_2);
-                                    push(operator_2);
-                                    break;
-                            default:
-                                    printf("Unknown named operation\n");
-                                    break;
-                        }
-                        break;
-                    case '+':
-                        push(pop() + pop());
-                        break;
-                    case '-':
-                        operator_2 = pop();
-                        push(pop() - operator_2);
-                        break;
-                    case '*':
-                        push(pop() * pop());
-                        break;
-                    case '/':
-                        operator_2 = pop();
-                        if (operator_2 != 0.0)
-                            push(pop() / operator_2);
-                        else
-                            printf("error: zero divisor\n");
-                        break;
-                    default:
-                        printf("Unknown command\n");
-                        break;
-                    }
-                } /* for */
-                if (e == '\n') /* print result */
-                    printf("\t%.8g\n", pop());
+            enum operations opcode = getOperationCode(string);
+            switch (opcode) {
+            case POWER:
+                operator_2 = pop();
+                push(pow(pop(), operator_2));
+                break;
+            case SIN:
+                push(sin(pop()));
+                break;
+            case COS:
+                push(cos(pop()));
+                break;
+            case EXP:
+                operator_2 = pop();
+                push(exp(pop()));
+                break;
+            case CLEAN:
+                stackPosition = 0;
+                break;
+            case DOUBLE:
+                operator_2 = pop();
+                push(operator_2);
+                push(operator_2);
+                break;
+            case SWAP:
+                operator_1 = pop();
+                operator_2 = pop();
+                push(operator_1);
+                push(operator_2);
+                break;
+            case TOP:
+                operator_2 = pop();
+                printf("\t%-.8g\n", operator_2);
+                push(operator_2);
+                break;
+            case ADD:
+                push(pop() + pop());
+                break;
+            case MULTIPLE:
+                push(pop() * pop());
+                break;
+            case DIFF:
+                operator_2 = pop();
+                push(pop() - operator_2);
+                break;
+            case DIVIDE:
+                operator_2 = pop();
+                if (operator_2 != 0.0)
+                    push(pop() / operator_2);
+                else
+                    printf("error:zero divisor\n");
+                break;
+            case MOD:
+                operator_2 = pop();
+                if (operator_2 != 0.0)
+                    push(fmod(pop(), operator_2));
+                else
+                    printf("error:zero divisor\n");
+                break;
+            default:
+                printf("error: unknown command %s\n", type);
+                break;
             }
+        }
+        if (e != ' ') {
+            printf("\t%-.8g\n", pop());
+        }
     }
     return 0;
 }
