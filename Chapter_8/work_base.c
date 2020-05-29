6fc17d1d8a67471811457e71dfaba947177845fe
vitalii <vitalii.iadrov@globallogic.com>
2020-05-29 17:19:46 +0300

    Add work_base.c

---
+++ Chapter_8/work_base.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define VARIANT_NUMBER 100
+#define BUF_LENGTH 20
+#define MAX_VARIANT 5
+
+typedef struct employee {
+    char *lastname;
+    int id;    
+    int year;
+    struct employee *next;
+} employee_t;
+
+void add_employee(employee_t **head, char *lastname, int id, int year);
+void find_emplyee_by_lastname(employee_t *head);
+void scan_employee_data(employee_t **head);
+void find_employee_by_id(employee_t *head);
+void print_employees(employee_t *head);
+int get_variant(int count);
+
+
+void scan_employee_data(employee_t **head) {
+	int id;
+	int year;
+	char lastname[BUF_LENGTH];
+
+    printf("Please enter last name for new employee: ");
+	scanf("%s", &lastname[0]);
+	printf("Please enter id for new employee: ");
+	scanf("%d", &id);
+	printf("Please enter year of birth for new employee: ");
+	scanf("%d", &year);
+	add_employee(head, lastname, id, year);
+}
+
+void add_employee(employee_t **head, char *lastname, int id, int year) {
+    employee_t *e;
+    employee_t *tmp;
+ 
+    e = malloc(sizeof(struct employee)); 
+    e->lastname = strdup(lastname);
+    e->id = id;
+    e->year = year;
+    e->next = NULL;
+    tmp = *head;
+ 
+    if (tmp == NULL) {
+		*head = e;
+    } else {
+	   while (tmp->next != NULL) {
+	    	tmp = tmp->next;
+	    }
+        tmp->next = e;
+    }
+}
+
+void find_employee_by_lastname(employee_t *head) {
+	employee_t *tmp;
+    char temp_lastname[BUF_LENGTH];
+	tmp = head;
+
+    printf("Please enter employee's last name for finding: ");
+    scanf("%s", temp_lastname);
+
+	while (tmp != NULL && strcmp(tmp->lastname, temp_lastname)) {
+		tmp = tmp->next;
+	}
+	if (tmp != NULL) {
+        printf("| %20s | %11d | %4d |\n", tmp->lastname, tmp->id, tmp->year);
+        printf("+---------------------+-------------+------+\n");
+    } else {
+        printf("%s is not found\n", temp_lastname);
+    }
+}
+
+void find_employee_by_id(employee_t *head) {	
+	employee_t *tmp;
+    int temp_id;
+    tmp = head;
+	printf("Please enter employee's ID number for finding: ");
+	scanf("%d", &temp_id);
+
+	while (tmp != NULL && (tmp->id != temp_id)) {
+        tmp = tmp->next;
+    }
+    if (tmp != NULL) {
+        printf("| %20s | %11d | %4d |\n", tmp->lastname, tmp->id, tmp->year);
+        printf("+---------------------+-------------+------+\n");
+    } else {
+        printf("Emploee with id #%d is not found\n", temp_id);
+    }
+}
+
+void print_employees(employee_t *head) {
+	employee_t *tmp;
+    tmp = head;
+ 
+    printf("+---------------------+-------------+------+\n");
+    printf("|         Employee    |    ID       | Year |\n");
+    printf("+---------------------+-------------+------+\n");
+    
+    if (tmp == NULL) {
+        printf("|            No employees in list          |\n");
+    } else {
+        while (tmp != NULL) {
+            printf("| %19s | %11d | %4d |\n", tmp->lastname, tmp->id, tmp->year);
+            tmp = tmp->next;
+        } 
+    }
+    printf("+---------------------+-------------+------+\n");    
+}
+
+int get_variant(int count) {
+    int variant;
+    char s[VARIANT_NUMBER];
+    scanf("%s", s);
+
+    while (sscanf(s, "%d", &variant) != 1 || variant < 1 || variant > count) {
+        printf("Incorrect option. Please try again: ");
+        scanf("%s", s);
+    }
+
+    return variant;
+}
+
+void print_menu() {
+    printf("Employee database\n");    
+    printf("1. Add new employee\n");
+    printf("2. Print the list of employees \n");
+    printf("3. Find employee by lastname\n");
+    printf("4. Find employee by ID number\n");
+    printf("5. Exit\n");
+    printf(">");
+}
+
+int main() {
+    employee_t *ptr_head = NULL;
+    int var;
+    
+    do {
+        print_menu();
+
+        var = get_variant(MAX_VARIANT);
+
+        switch (var) {
+            case 1:
+                scan_employee_data(&ptr_head);
+                break;
+
+            case 2:
+                print_employees(ptr_head);
+                break;
+
+            case 3:
+                find_employee_by_lastname(ptr_head);
+                break;
+
+            case 4:
+                find_employee_by_id(ptr_head);
+                break;
+        }
+
+    } while (var != MAX_VARIANT);
+
+    return 0;
+}
